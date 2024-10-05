bd0667004076f46f3c3d0b160d5ef1991c0dbd54
moreno <moreno.ferreira11@gmail.com>
2024-10-04 22:47:11 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,19 @@
 </script>
 
 <template>
-<button type="button">C</button>
-<button type="button">()</button>
-<button type="button">%</button>
-<button type="button">:</button>
+  <div class="container">
+    <button type="button">C</button>
+    <button type="button">()</button>
+    <button type="button">%</button>
+    <button type="button">:</button>
+  </div>
+  <div class="container">
+    <button type="button">7</button>
+    <button type="button">8</button>
+    <button type="button">9</button>
+    <button type="button">x</button>
+  </div>
+
 </template>
 
 <style scoped>
