b77310617c08740f1ccd573653f1b13a240d2c26
moreno <moreno.ferreira11@gmail.com>
2024-10-08 11:43:32 -0300

    funcao e complemtenros

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
-<script setup>
-</script>
-
 <template>
-  <div class="container">
-    <button type="button">C</button>
-    <button type="button">()</button>
-    <button type="button">%</button>
-    <button type="button">:</button>
+  <div class="container mt-5">
+    <div class="row">
+      <h1 class="text-center mb-4">Calculadora Aritmética</h1>
+      <div class="col-md-4">
+        <h3>Primeiro Número</h3>
+        <input type="number" v-model="num1" placeholder="Primeiro Número" class="form-control" />
+      </div>
+      <div class="col-md-4">
+        <h3>Escolha a operação</h3>
+        <select class="form-control" v-model="operacao">
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">x</option>
+          <option value="%">%</option>
+        </select>
+      </div>
+      <div class="col-md-4">
+        <h3>Segundo Número</h3>
+        <input type="number" v-model="num2" placeholder="Segundo Número" class="form-control" />
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col text-center">
+        <h2>Resultado: {{ resultado }}</h2>
+      </div>
+    </div>
   </div>
-  <div class="container">
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button">x</button>
-  </div>
-
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operacao: '+',
+    };
+  },
+  computed: {
+    resultado() {
+      switch (this.operacao) {
+        case '+':
+          return this.num1 + this.num2;
+        case '-':
+          return this.num1 - this.num2;
+        case '*':
+          return this.num1 * this.num2;
+        case '%':
+          return (this.num1 * this.num2) / 100;
+        default:
+          return 0;
+      }
+    },
+  },
+};
+</script>
 
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: auto;
+}
 </style>
